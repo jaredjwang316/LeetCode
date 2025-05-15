113890bd6781680ae71c42000bdc69ebf6e471fa
Jared Wang <jwangj001@gmail.com>
2025-05-15 17:47:15 -0500

    Added test cases by myself for linked list palindrome problem

--- Linked_Lists/IsPalindrome_Linked_List.cpp
+++ Linked_Lists/IsPalindrome_Linked_List.cpp
@@ -32,3 +32,44 @@ bool isPalindrome(ListNode* head) {
     }
     return true;
 }
+
+int main() {
+    // Create the linked list: 1 -> 2 -> 2 -> 1
+    ListNode* node4 = new ListNode(1);
+    ListNode* node3 = new ListNode(2, node4);
+    ListNode* node2 = new ListNode(2, node3);
+    ListNode* node1 = new ListNode(1, node2);
+    ListNode* head = node1;
+
+    // Test isPalindrome
+    if (isPalindrome(head)) {
+        cout << "The linked list is a palindrome." << endl;
+    } else {
+        cout << "The linked list is not a palindrome." << endl;
+    }
+
+    // Clean up memory
+    delete node1;
+    delete node2;
+    delete node3;
+    delete node4;
+
+    // ---- Test Case 2: Not a Palindrome ----
+    ListNode* m3 = new ListNode(3);
+    ListNode* m2 = new ListNode(2, m3);
+    ListNode* m1 = new ListNode(1, m2);
+    ListNode* head2 = m1;
+
+    if (isPalindrome(head2)) {
+        cout << "Test 2: The linked list is a palindrome." << endl;
+    } else {
+        cout << "Test 2: The linked list is not a palindrome." << endl;
+    }
+
+    // Clean up memory
+    delete m1; 
+    delete m2; 
+    delete m3;
+    
+    return 0;
+}
