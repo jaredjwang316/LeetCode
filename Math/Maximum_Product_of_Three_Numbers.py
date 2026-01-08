e3ae6a74891572a4a24e734572d9e750ce53af1a
jaredjwang316 <jwangj001@tamu.edu>
2026-01-08 16:57:34 -0600

    Maximum product of three numbers in an given array

---
+++ Math/Maximum_Product_of_Three_Numbers.py
@@ -0,0 +1,38 @@
+"""
+Problem Description:
+    Given an integer array nums, find three numbers whose product is maximum and return the maximum product.
+
+Constraints:
+    - 3 <= nums.length <= 10^4
+    - -1000 <= nums[i] <= 1000
+"""
+
+def maximumProduct(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    nums.sort()
+    if nums[0] < 0 and nums[1] < 0 and nums[-1] > 0:
+        return max(nums[0] * nums[1] * nums[-1], nums[-3] * nums[-2] * nums[-1]) 
+    
+    return nums[-3] * nums[-2] * nums[-1]
+
+nums = [1,2,3]
+print(maximumProduct(nums)) # 6
+
+nums2 = [1,2,3,4]
+print(maximumProduct(nums2)) # 24
+
+nums3 = [-1,-2,-3]
+print(maximumProduct(nums3)) # -6
+
+nums4 = [-1,-2,-3,-4]
+print(maximumProduct(nums4)) # -6
+
+nums5 = [-710,-107,-851,657,-14,-859,278,-182,-749,718,-640,127,-930,-462,694,969,143,309,904,-651,160,
+         451,-159,-316,844,-60,611,-169,-73,721,-902,338,-20,-890,-819,-644,107,404,150,-219,459,-324,
+         -385,-118,-307,993,202,-147,62,-94,-976,-329,689,870,532,-686,371,-850,-186,87,878,989,-822,-350,
+         -948,-412,161,-88,-509,836,-207,-60,771,516,-287,-366,-512,509,904,-459,683,-563,-766,-837,-333,
+         93,893,303,908,532,-206,990,280,826,-13,115,-732,525,-939,-787]
+print(maximumProduct(nums5)) # 972256230
